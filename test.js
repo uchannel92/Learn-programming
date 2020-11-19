926ad13f26ca4c4a9aae61a5dce39e89f96a876b
Uchannel92 <pablo1@gmail.com>
2020-11-19 12:16:19 +0000

    Added a new instance -Dom- to the class User on test.js

---
+++ test.js
@@ -0,0 +1,52 @@
+// class definition
+
+export default class Employees {
+    constructor(firstName, lastName, email, position, refId, employeeStatus, age){
+        console.log("New instance has been created");
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.position = position;
+        this.refId = refId;
+        this.employeeStatus = employeeStatus;
+        this.age = age;
+    }
+
+        getFullName(){
+            return `${this.firstName} ${this.lastName}`
+        }
+
+        getPositionAndEmployeeStatus(){
+            return  this.position + " " + this.employeeStatus
+        }
+
+        getEmail(){
+            return `${this.email}`
+        }
+
+        getAge(){
+            return this.age
+        }
+
+        canVote(){
+            
+            if (this.age >= 21) {
+                return "You can Vote"
+            } else {
+                return "You cannot vote"
+            }   
+        }
+        
+        isPerm(){
+            if (this.employeeStatus === "Permanent" || this.position === "Manager") {
+                return true
+            } else {
+                return false
+            }
+        }
+
+        getInitials(){
+            return `${this.firstName[0]}${this.lastName[0]}`
+        }
+}
+
